32dda23fc777e5ca88a49424b4985d72ba31666f
RICCOs <agaki08@internet.ru>
2023-04-15 10:34:23 +0300

    Added General menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject menu;
+    public GameObject settingsmenu;
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Settings()
+    {
+        menu.SetActive(false);
+        settingsmenu.SetActive(true);
+    }
+
+    public void BackSettings()
+    {
+        settingsmenu.SetActive(false);
+        menu.SetActive(true);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
