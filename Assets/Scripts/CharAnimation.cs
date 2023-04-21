cb8d37f25aa98ff4421f09e15e7fd8bf72ae5ac5
RICCOs <agaki08@internet.ru>
2023-04-21 22:24:58 +0300

    Added Animations: Run, Jump, Fall; Transitions between anim. Idle and Run. (Unfinished Jump and Fall, Transitions between them.

---
+++ Assets/Scripts/CharAnimation.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharAnimation : MonoBehaviour
+{
+    private Animator _animator;
+    public bool IsMoving { private get; set; }
+    public bool IsFlying { private get; set; }
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        _animator.SetBool("isMoving", IsMoving);
+    }
+
+    public void Jump()
+    {
+        _animator.SetTrigger("Jump");
+    }
+}
--- Assets/Scripts/MovementChar.cs
+++ Assets/Scripts/MovementChar.cs
@@ -4,29 +4,89 @@ using UnityEngine;
 
 public class MovementChar : MonoBehaviour
 {
-    [SerializeField] private float _speed = 10;
+    [SerializeField] private float _speed = 10f;
     [SerializeField] private SpriteRenderer _spriteChar;
+    [SerializeField] private float _jumpForce = 4f;
+    [SerializeField] private Vector3 _groundCheckOffset;
     private Vector3 _input;
     private Rigidbody2D _rigidbody;
+    private CharAnimation _animations;
+    private bool _isMoving;
+    private bool _isGrounded;
+    private bool _isFlying;
+
+    public LayerMask groundMask;
+
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _animations = GetComponentInChildren<CharAnimation>();
     }
 
     
     private void FixedUpdate()
     {
         Move();
+        CheckGround();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            if(_isGrounded)
+            {
+                Jump();
+                _animations.Jump();
+            }
+        }
+
+        _animations.IsMoving = _isMoving;
+        _animations.IsFlying = IsFlying();
     }
 
     private void Move()
     {
         _input = new Vector3(Input.GetAxis("Horizontal"), 0);
         transform.position += _input * _speed * Time.deltaTime;
+        _isMoving = _input.x != 0 ? true : false;
 
-        if(_input.x != 0)
+        if(_isMoving)
         {
             _spriteChar.flipX = _input.x > 0 ? false : true;
+        }
+
+        _animations.IsMoving = _isMoving;
+    }
+
+    private void Jump()
+    {
+        Debug.Log("Jump");
+        _rigidbody.AddForce(transform.up * _jumpForce, ForceMode2D.Impulse);
+    }
+
+    private void CheckGround()
+    {
+        float rayLenth = 0.3f;
+        Vector3 rayStartPosition = transform.position + _groundCheckOffset;
+        RaycastHit2D hit = Physics2D.Raycast(rayStartPosition, rayStartPosition + Vector3.down, rayLenth, groundMask);
+
+        if (hit.collider != null)
+        {
+            _isGrounded = hit.collider.CompareTag("Ground") ? true : false;
+        }
+        else
+        {
+            _isGrounded = false;
+        }
+    }
+
+    private bool IsFlying()
+    {
+        if(_rigidbody.velocity.y < 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 }
